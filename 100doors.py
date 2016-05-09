b040476765d80b9bbd5f15d4aa10d7c02ebd608f
Adam <horada1991@gmail.com>
2016-05-09 10:52:48 +0200

    Comments added

--- 100doors.py
+++ 100doors.py
@@ -1,7 +1,9 @@
 doors = [0]*100
 j = 1
+#Loop for increase steps
 while j <= 100:
     i = 0
+    #Loop for check doors and close or open them (closed = 0, opened = 1)
     while i < len(doors):
         if doors[i] == 0:
             doors[i] = 1
@@ -10,6 +12,7 @@ while j <= 100:
         i += j
     j += 1
 
+#Printing out the opened doors's numbers
 OpenedDoors = []
 for i in range(len(doors)):
     if doors[i] == 1:
